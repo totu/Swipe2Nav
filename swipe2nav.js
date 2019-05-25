49ee752cc55fecf8a6bbe69f0f2717aa6368c9b4
totu <topituulensuu@fastmail.com>
2019-05-25 14:34:52 +0300

    require longer swipe to navigate
    Previous version started to navigate immediately after it detected a
    side ways scrolling gesture. This version requires a more deliberate
    movement in order to trigger navigation.
    
    Also slight refactoring. Now everything isn't inside one giant function.

--- swipe2nav.js
+++ swipe2nav.js
@@ -1,44 +1,90 @@
-// Main navigation function
-// Event is scroll event from jquery.mousewheel
-// From that we can determine which direction we are scrolling
-const navigate = function(event)
+// Released under MIT License
+var sensitivity = 25;
+var timeout = false;
+var movement = 0;
+var clearMovementTimer = null;
+
+// Modify movement variable and handle its reset
+const handleMovement = function(direction)
+{
+    // Clear existing reset timer
+    clearTimeout(clearMovementTimer);
+    // Set up new reset timer
+    clearMovementTimer = setTimeout(function() { movement = 0; }, 800);
+    // Modify movement
+    movement += direction;
+}
+
+
+// Return direction for `history.go()` function
+const figureoutDirection = function(event)
 {
     const deltaX = event.deltaX;
     const deltaY = event.deltaY;
-    let goDir = NaN;
+    let direction = NaN;
 
-    // Check for horizontal scrolling;
-    if (Math.abs(deltaX) > 0) {
+    // Check we are horizontal scrolling and not vertically
+    if (Math.abs(deltaX) > 0 && Math.abs(deltaY) == 0)
+    {
+        // Go forwards
+        if (deltaX > 0)
+            direction = 1;
 
-        // Check that we are not scrolling vertically and set direction
-        if (deltaX > 0 && Math.abs(deltaY) == 0)
-        {
-            // Go forwards
-            goDir = 1;
-        }
-
-        else if (deltaX < 0 && Math.abs(deltaY) == 0)
-        {
-            // Go forwards
-            goDir = -1;
-        }
+        // Go forwards
+        else if (deltaX < 0)
+            direction = -1;
     }
 
-    // If something set direction then disable scroll nav and go
-    // Disabling is done for 500ms so there wont be accidental navigations
-    if (goDir != NaN)
+    return direction;
+}
+
+
+// Main navigation function
+const navigate = function(event)
+{
+    // Event is scroll event from jquery.mousewheel
+    goDir = figureoutDirection(event);
+    // If direction is set disable scroll navigation and go
+    if (!isNaN(goDir))
     {
-        $(document).off("wheel");
-        history.go(goDir);
-        setTimeout(function() {enableNav();}, 500);
+        handleMovement(goDir);
+        if (Math.abs(movement) > sensitivity)
+        {
+            // Hands off mouse events
+            $(document).off("wheel");
+            // Check we are not waiting on timeout
+            if (!timeout)
+            {
+                // Lock out timer
+                timeout = true;
+                // Navigate
+                history.go(goDir);
+                //// console.log(goDir);
+                //// console.log(movement);
+                // Re-enable navigation after 500ms to prevent accidental gestures
+                setTimeout(function() { enableNav() }, 500);
+            }
+        }
     }
 }
 
-// Hook navigate function to mouse movement
+
+// Enable addon functionality
 const enableNav = function()
 {
+    // Hook navigate function to mouse movement
     $(document).on('mousewheel', navigate);
+    // Reset timeout boolean
+    clearTimeout(clearMovementTimer);
+    movement = 0;
+    timeout = false;
 }
+
 
 // First run so we can get it working
 enableNav();
+
+// Just incase some pages are doing weird things
+$(document).ready(function() {
+    enableNav();
+});
